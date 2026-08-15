d27a850b37361f6b1ab5afc358eabadb216fc6e7
priyanshu620-len <dwivedisantosh317@gmail.com>
2026-08-16 00:37:39 +0530

    Fix asyncio event loop setup in bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
-import os
+import asyncio
+try:
+    asyncio.get_event_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop()) import os  
 import re
 import gc
 import time
